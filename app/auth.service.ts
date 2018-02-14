10826afbe81b67a984c56334e1442e0d62b5bec1
Unknown <klervi29@hotmail.com>
2018-02-14 15:21:50 +0100

    authservice commenté

--- app/auth.service.ts
+++ app/auth.service.ts
@@ -7,14 +7,16 @@ import { Utilisateur } from './utilisateur';
 
 @Injectable()
 export class AuthService {
+
+	
 	
 	constructor(private appConfig: AppConfigService, private http: Http) {}
 	
 	public findByUsername(username: string) {
-		var token: string;
-		this.http.post(this.appConfig.uri + "/auth", { username: "nom_user" })
-			.subscribe(resp => this.token = resp.json());
-		console.log(this.token);
+//		var token: string;
+//		this.http.post(this.appConfig.uri + "/auth", { username: "nom_user" })
+//			.subscribe(resp => this.token = resp.json());
+//		console.log(this.token);
 	}
 	
 }
