e3f88448e94aa25d7ad2a731b7694d2ecf0a65ef
Unknown <klervi29@hotmail.com>
2018-02-14 16:19:43 +0100

    Klervi 9 (redirection apres authentification)

--- app/auth.component.ts
+++ app/auth.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 import { AuthService } from './auth.service';
 import { Utilisateur } from './utilisateur';
 
@@ -12,12 +14,12 @@ export class AuthComponent {
 	
 	private utilisateur : Utilisateur = new Utilisateur();
 	
-	constructor(private service: AuthService) {}
+	constructor(private service: AuthService, private router: Router) {}
 	
 	public valider() {
 		this.service.findByUsername(this.utilisateur.username);
-		
-		this.utilisateur = new Utilisateur();	
+		this.router.navigate(['/canal']);
+//		this.utilisateur = new Utilisateur();
 	}
 	
 }
