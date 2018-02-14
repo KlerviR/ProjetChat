808cb4b3c627c485805b55d982f53040d5834b2f
Unknown <chris_bel@hotmail.fr>
2018-02-14 12:05:31 +0100

    app-config.service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppConfigService } from './app-config.service';
 
 
 @NgModule({
     imports: [
         BrowserModule
+    ],
+
+    providers: [
+        AppConfigService
     ],
 
     declarations: [
